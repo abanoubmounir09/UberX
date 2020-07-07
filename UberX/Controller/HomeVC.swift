44d527c0daa6a2277467325d6c982f277227de3a
pop <pop@pops-Mac.local>
2020-07-07 05:37:52 -0700

    add rout and destination

--- UberX/Controller/HomeVC.swift
+++ UberX/Controller/HomeVC.swift
@@ -50,27 +50,28 @@ class HomeVC: UIViewController,Alertable {
         revealingSplashView.animationType = .heartBeat
         revealingSplashView.startAnimation()
         revealingSplashView.heartAttack = true
+        
         //MARK:- retrn completion about available trips
-        UpdateService.instance.observeTrips { (tripDict) in
-            if tripDict != nil{
-                let pickupCoordinateArray = tripDict["pickupCoordinate"] as?  NSArray
-                    let tripKey = tripDict["passengerKey"] as? String
-                    let acceptedStatus = tripDict["tripIsAccepted"] as? Bool
-                if acceptedStatus == false{
-                    DataService.instance.driverIsAvailable(key: self.currentUser!) { (available) in
-                        if let avail = available{
-                            if avail == true{
-                                let storyBoard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
-                                let pickUpVC = storyBoard.instantiateViewController(withIdentifier: "PickUpVC") as? PickUpVC
-                                pickUpVC?.initData(coordinate: CLLocationCoordinate2D(latitude: pickupCoordinateArray![0] as! CLLocationDegrees, longitude: pickupCoordinateArray![1] as! CLLocationDegrees), pasengerKey: tripKey!)
-                                self.present(pickUpVC!, animated: true, completion: nil)
-                            }
-                           
-                        }
-                    }
-                }
-            }
-        }
+//        UpdateService.instance.observeTrips { (tripDict) in
+//            if tripDict != nil{
+//                let pickupCoordinateArray = tripDict["pickupCoordinate"] as?  NSArray
+//                    let tripKey = tripDict["passengerKey"] as? String
+//                    let acceptedStatus = tripDict["tripIsAccepted"] as? Bool
+//                if acceptedStatus == false{
+//                    DataService.instance.driverIsAvailable(key: self.currentUser!) { (available) in
+//                        if let avail = available{
+//                            if avail == true{
+//                                let storyBoard = UIStoryboard.init(name: "Main", bundle: Bundle.main)
+//                                let pickUpVC = storyBoard.instantiateViewController(withIdentifier: "PickUpVC") as? PickUpVC
+//                                pickUpVC?.initData(coordinate: CLLocationCoordinate2D(latitude: pickupCoordinateArray![0] as! CLLocationDegrees, longitude: pickupCoordinateArray![1] as! CLLocationDegrees), pasengerKey: tripKey!)
+//                                self.present(pickUpVC!, animated: true, completion: nil)
+//                            }
+//
+//                        }
+//                    }
+//                }
+//            }
+//        }
         
     }
     
@@ -128,6 +129,7 @@ class HomeVC: UIViewController,Alertable {
          let cordinateRegion = MKCoordinateRegion(center: MapView.userLocation.coordinate, latitudinalMeters: regionRadios * 2, longitudinalMeters: regionRadios * 2)
          MapView.setRegion(cordinateRegion, animated: true)
     }
+    
     //MAARK:- Action btn to requset trip
     @IBAction func RequestBTNwasPressed(_ sender: Any) {
         UpdateService.instance.updateTripWithCoordinateUponREquest()
